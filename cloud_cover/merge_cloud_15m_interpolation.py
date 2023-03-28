7c9f80157f39df8ba9ee220218cf561ec54b0859
Skin1215 <93082577+Skin1215@users.noreply.github.com>
2023-03-28 20:32:23 +0800

    Update merge_cloud_15m_interpolation.py

--- cloud_cover/merge_cloud_15m_interpolation.py
+++ cloud_cover/merge_cloud_15m_interpolation.py
@@ -59,7 +59,7 @@ df2 = df2.reset_index(drop=True)
 df_all = pd.concat([dfXlen_result,df2],axis=1)
 
 #存成csv
-df_all.to_csv(f'./dataset/10m_to_15m_colud.csv', index=None)
+df_all.to_csv(f'./dataset/merge_10m_to_15m_colud.csv', index=None)
 
 
 # In[ ]:
